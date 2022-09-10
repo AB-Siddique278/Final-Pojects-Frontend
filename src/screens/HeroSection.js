0b34100459a7446964c5d34c833beb4e68ff2a0d
AB-Siddique278 <absiddique1060@gmail.com>
2022-09-11 01:46:00 +0600

    featch data from server

--- src/screens/HeroSection.js
+++ src/screens/HeroSection.js
@@ -1,40 +1,42 @@
+
 import React from 'react';
 
 const HeroSection = () => {
+    
     return (
         <div>
-        <div className='container'>
+            <div className='container'>
 
-<div className='row my-5'>
-
-
-    <dir className='col-md-6 d-flex align-items-center'>
-        <div>
-            <h1 > Wellcome To our Megaann Food</h1>
-            <h2>We sell all kinds of Food </h2>
-            <p>
-                Supplying our customers with the freshest, highest quality fruit.
-            </p>
-
-            <p>Processing is essential to ensure safety when canning fruits. Fruits, being acidic foods, can be processed safely in a boiling-water bath. However, some people prefer to pressure-process fruits.. </p>
-            <button type="button" class="btn btn-warning btn-lg">Learn More</button>
-        </div>
-    </dir>
+                <div className='row my-5'>
 
 
-    <div className='col-md-6'>
+                    <dir className='col-md-6 d-flex align-items-center'>
+                        <div>
+                            <h1 > Wellcome To our Megaann Food</h1>
+                            <h2>We sell all kinds of Food </h2>
+                            <p>
+                                Supplying our customers with the freshest, highest quality fruit.
+                            </p>
 
-        <div className=''>
-
-            <img className='img-fluid' src="https://img.freepik.com/free-photo/delicious-fried-chicken-plate_144627-27383.jpg?w=996&t=st=1661271716~exp=1661272316~hmac=dbce6638cd4abbd6194f76eee4a51cdb938c75a0d629175e3cb6fbc6098909ba" alt="" />
-
-        </div>
-    </div>
+                            <p>Processing is essential to ensure safety when canning fruits. Fruits, being acidic foods, can be processed safely in a boiling-water bath. However, some people prefer to pressure-process fruits.. </p>
+                            <button type="button" class="btn btn-warning btn-lg">Learn More</button>
+                        </div>
+                    </dir>
 
 
-</div>
+                    <div className='col-md-6'>
 
-</div>
+                        <div className=''>
+
+                            <img className='img-fluid' src="https://img.freepik.com/free-photo/delicious-fried-chicken-plate_144627-27383.jpg?w=996&t=st=1661271716~exp=1661272316~hmac=dbce6638cd4abbd6194f76eee4a51cdb938c75a0d629175e3cb6fbc6098909ba" alt="" />
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,35 +1,45 @@
 import { Link } from "react-router-dom";
-import data from "../data";
+
 import HeroSection from "./HeroSection";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-
-function HomeScreen(){
+function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios.get('/api/products');
+        setProducts(result.data);
+      };
+      fetchData();
+    }, []);
     return <div>
-            <HeroSection></HeroSection>
-            <h1 className='text-center'> List products</h1>
-            <div className=" products">
+        <HeroSection></HeroSection>
+        <h1 className='text-center'> List products</h1>
+        <div className=" products">
 
 
-                {
-                    data.products.map((product) => (
-                        <div className="card product" key={product.slug}>
+            {
+                products.map((product) => (
+                    <div className="card product" key={product.slug}>
+                        <Link to={`/product/${product.slug}`}>
+                            <img className="card-img-top" src={product.image} alt={product.name} />
+                        </Link>
+
+                        <div className='product-info'>
+
                             <Link to={`/product/${product.slug}`}>
-                                <img className="card-img-top" src={product.image} alt={product.name} />
+                                <p>{product.name} </p>
                             </Link>
 
-                            <div className='product-info'>
+                            <p>{product.price}</p>
+                            <button type="button" class="btn btn-warning ">Add to Card</button>
+                        </div>
+                    </div>))
+            }
 
-                                <Link to={`/product/${product.slug}`}>
-                                    <p>{product.name} </p>
-                                </Link>
-
-                                <p>{product.price}</p>
-                                <button type="button" class="btn btn-warning ">Add to Card</button>
-                            </div>
-                        </div>))
-                }
-
-            </div>
+        </div>
     </div>
 }
 export default HomeScreen;
